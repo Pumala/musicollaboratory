7c08e655d5bcc63cb2a6d93d61a18ffe2e4ea5a0
Carolyn Lam <caro_lyn_@hotmail.com>
2016-12-19 14:28:46 -0500

    add additional files

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,26 @@
 var app = angular.module('music_app', ['ui.router', 'ngCookies']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state({
+    name: 'home',
+    url: '/',
+    templateController: 'templates/home.html',
+    controller: 'HomeController'
+  }),
+  .state({
+    name: 'login',
+    url: '/login',
+    templateController: 'templates/login.html',
+    controller: 'LoginController'
+  })
+  .state({
+    name: 'signup',
+    url: '/signup',
+    templateController: 'templates/signup.html',
+    controller: 'SignupController'
+  })
+
+  $urlRouterProvider.otherwise('/');
+
+})
